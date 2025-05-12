673823f4a94850e17dfbc4c8157801ef0c9f4a79
ethan <ethanjupi@gmail.com>
2025-05-11 18:00:28 -0600

    data upload and details for api

--- src/posts/posts.routes.js
+++ src/posts/posts.routes.js
@@ -2,12 +2,12 @@ import { Router } from "express"
 import { addPostValidator, getPostValidator } from "../middlewares/posts-validator.js"
 import { getPosts, createPost, deletePost, lookForPost} from "./posts.controller.js"
 
-const router = Router();
+const router = Router();    
 
 router.post('/addPosts', addPostValidator, createPost)
-router.get('/getPosts', getPosts)
+router.post('/getPosts', getPosts)
 router.delete('/deletePosts/:id', deletePost)
-router.get('/getPosts/:id', getPostValidator, lookForPost)
+router.get('/getPostsById/:id', getPostValidator, lookForPost)
 
 export default router
 
